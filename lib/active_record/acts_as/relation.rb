098b5405c6d74275ba8035fa9e88b0f7cf6c422d
WANG QIANG <rovingbreeze@gmail.com>
2016-10-21 15:20:07 +0800

    Use dependent: :destroy for the acting_as class

--- lib/active_record/acts_as/relation.rb
+++ lib/active_record/acts_as/relation.rb
@@ -73,7 +73,7 @@ module ActiveRecord
         def actable(options = {})
           name = options.delete(:as) || :actable
 
-          reflections = belongs_to name, {polymorphic: true, dependent: :delete}.merge(options)
+          reflections = belongs_to name, {polymorphic: true, dependent: :destroy}.merge(options)
 
           cattr_reader(:actable_reflection) { reflections.stringify_keys[name.to_s] }
 
